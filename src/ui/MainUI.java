bfbf00b102e33f15b559130b4654c9947d511c7b
PoghosPertrosyan <poghos.petrosyan92@gmail.com>
2018-06-18 11:53:08 +0400

    Adding MainUI without finctionality

---
+++ src/ui/MainUI.java
@@ -0,0 +1,18 @@
+package ui;
+
+import javafx.event.ActionEvent;
+
+public class UserUI {
+
+    public UserUI() {
+    }
+
+    public void addAirplane(ActionEvent actionEvent) {
+    }
+
+    public void editArplane(ActionEvent actionEvent) {
+    }
+
+    public void deleteArplane(ActionEvent actionEvent) {
+    }
+}
